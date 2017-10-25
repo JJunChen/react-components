278b209c2cfcb5f5e4bdc271c595278738c9c83f
Jiajun Chen <jchen0318@gmail.com>
2017-10-25 13:20:40 -0700

    Complete react-componenet

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// TODO
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hover: false
+		};
+	}
+
+	onListItemHover() {
+		this.setState({
+			hover: true
+		});
+	};
+	onListItemExistHover() {
+		this.setState({
+			hover: false
+		});
+	};
+	render() {
+		var style = {'fontWeight': this.state.hover ? 'bold' : 'normal'};
+		return (
+			<li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemExistHover.bind(this)}>{this.props.item}</li>
+		);
+	}
+};
+
+var CreateGroceryList = (props) => (
+	<ul>
+		{props.items.map(item => 
+			<GroceryListItem item = {item} />
+		)}
+	</ul>
+);
+
+var GroceryList = () => (
+	<div>
+		<CreateGroceryList items = {['cucumbers', 'kale']}/>
+	</div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
